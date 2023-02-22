9ff116309f4ab2baf22c89490a74b327156de894
JateX <grisaia@inbox.ru>
2023-02-22 20:32:20 +0300

    Initial commit

---
+++ Task_1/calculator.cpp
@@ -0,0 +1,5 @@
+#include "calculator.h"
+	
+int calculator(int num1, int num2) {
+	return 0;
+}
---
+++ Task_1/calculator.h
@@ -0,0 +1,6 @@
+#ifndef CALCULATOR
+#define CALCULATOR
+
+int calculator(int num1, int num2);
+
+#endif
